c797026964b8ecc74ff8efcbecb0cb66af6a135f
chelebyy <muhammett.celebi@gmail.com>
2024-10-07 00:55:47 +0300

    .

---
+++ 03_MakingDesicion/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _03_MakingDesicion
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            #region if else
+
+            //Console.WriteLine("Lütfen Şifrenizi Giriniz");
+            //string password;
+            //password = Console.ReadLine();
+
+            //if (password == "abcd")
+            //{
+            //    Console.WriteLine("Şifre doğru");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Şifre Yanlış");
+            //}
+
+            //string capital, country;
+            //Console.Write("Başkenti Giriniz : ");
+            //capital = Console.ReadLine();
+            //Console.Write("Ülkeyi Gieriniz :");
+            //country = Console.ReadLine();
+
+            //if (capital == "ankara" & country == "türkiye")
+            //{
+            //    Console.Write("Veriler doğrulandı");
+            //}
+
+            //else { 
+            
+            //Console.Write("Veriler Doğrulanamadı");
+            //}
+            Console.Read();
+            #endregion
+        }
+    }
+}
